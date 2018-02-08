67ce79b51b84a1dff2a3b081e5d708f1162042e5
Helen Ye <00yhelen@gmail.com>
2018-02-08 13:06:33 -0500

    Add dvd animation

--- animate.js
+++ animate.js
@@ -1,47 +1,80 @@
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
+var funB = document.getElementById("fun");
+var dvdB = document.getElementById("dvd");
 var stopB = document.getElementById("stop");
 
 var h = c.getAttribute("height");
 var w = c.getAttribute("width");
 var r = h / 25;
 
-var x = -1;
-var y = -1;
+var x = null;
+var y = null;
+var xChange = 1;
+var yChange = 2;
+var MAX_CHANGE = 4;
 
 var pi = Math.PI;
 
 var a = -1;
+var b = -1;
 
-var drawCircle = function(e) {
-    ctx.clearRect(0, 0, h, w);
-    if (x < 0 || y < 0) {
-        x = e.offsetX;
-        y = e.offsetY;
-    }
+var drawCircle = function(cx, cy) {
     ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2 * pi);
+    ctx.arc(cx, cy, r, 0, 2 * pi);
     ctx.strokeStyle = "#000000";
     ctx.stroke();
     ctx.fillStyle = "#ff0000";
     ctx.fill();
-    x++;
-    a = window.requestAnimationFrame(drawCircle);
+}
+
+var animateDVD = function(e) {
+    if (b > -1) {
+        stopFrame(b);
+    }
+    clear();
+    if (x == null || y == null) {
+        x = w / 2;
+        y = h / 2;
+    }
+    drawCircle(x, y);
+    x += xChange;
+    y += yChange;
+    if (x > w - r) {
+        xChange = Math.floor(Math.random() * MAX_CHANGE) * -1;
+    } else if (x < r) {
+        xChange = Math.floor(Math.random() * (MAX_CHANGE)) + 1;
+    }
+    if (y > h - r) {
+        yChange = Math.floor(Math.random() * MAX_CHANGE) * -1;
+    } else if (y < r) {
+        yChange = Math.floor(Math.random() * (MAX_CHANGE)) + 1;
+    }
+    console.log(xChange + "," + yChange);
+    a = window.requestAnimationFrame(animateDVD);
 }
 
 var reset = function() {
-    x = -1;
-    y = -1;
+    x = null;
+    y = null;
     a = -1;
+    b = -1;
 }
 
-var stopCanvas = function(e) {
-    e.preventDefault();
-    if (a > -1) {
-        window.cancelAnimationFrame(a);
-    }
+var clear = function() {
+    ctx.clearRect(0, 0, h, w);
+}
+
+var stopFrame = function(frame) {
+    window.cancelAnimationFrame(frame);
     reset();
 }
 
-c.addEventListener('click', drawCircle);
+var stopCanvas = function(e) {
+    stopFrame(a);
+    stopFrame(b);
+    reset();
+}
+
+dvdB.addEventListener('click', animateDVD);
 stopB.addEventListener('click', stopCanvas);
